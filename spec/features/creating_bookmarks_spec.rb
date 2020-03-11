a3ce6e423e5773240dd21aa6d50d9841ac05ac7e
Ion <ion.vizitiu88@gmail.com>
2020-03-11 16:44:24 +0000

    Wrapping returned data

--- spec/features/creating_bookmarks_spec.rb
+++ spec/features/creating_bookmarks_spec.rb
@@ -2,8 +2,11 @@ feature 'Adding a new bookmark' do
   scenario 'a user can add a bookmark to BookmarkManager' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://testbookmark.com')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
 
-    expect(page).to have_content 'http://testbookmark.com'
+    expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
   end
+
+
 end
